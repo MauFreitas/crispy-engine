0519c7dcb8a466376da3210d5f6aa5c5c430c46f
Mauricio <mauriciop108@gmail.com>
2022-05-02 22:37:16 -0300

    Total de Aulas

---
+++ mercado.js
@@ -0,0 +1,22 @@
+/*
+const item1 = "detergente \n"
+const item2 = "sabonete \n"
+const item3 = "pasta de dente \n"
+var totalDeProdutos = item1+item2+item3
+console.log("Produtos em Estoque: \n"+totalDeProdutos)
+*/
+const NumeroDeAulas = 1149
+
+const duracaoDaAula = 15
+
+var total = NumeroDeAulas*duracaoDaAula
+
+var minutos = 60
+
+var resultado = total / 60
+
+var resultadoSegundos = resultado / 60
+
+var resultadoEmDias = resultado / 24
+
+console.log("Horas de aula: ",resultado,"\n Total em segundos: ",resultadoSegundos,"\n Total em Dias: ",resultadoEmDias)
